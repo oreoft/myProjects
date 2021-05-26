1c021d2eb94d328412840abee3bf66288a0f04ea
oreoft <ft1216@foxmail.com>
2021-05-26 10:03:42 +0800

    chore(maven): reinit
    chore(maven): reinit

---
+++ mix-tools/src/test/java/cn/someget/mixtools/MixToolsApplicationTests.java
@@ -0,0 +1,13 @@
+package cn.someget.mixtools;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MixToolsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
